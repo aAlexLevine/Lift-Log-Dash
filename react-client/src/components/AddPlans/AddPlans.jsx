5ccaf3003eaf71b225a046432ebe0a67cc2b8192
Alex Levine <alexlevine@Alexs-MBP-2.home>
2019-05-03 01:48:36 -0400

    Add/remove groups and exercises to new plan form, immutable-helper installed

--- react-client/src/components/AddPlans/AddPlans.jsx
+++ react-client/src/components/AddPlans/AddPlans.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Paper from '@material-ui/core/Paper';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardHeader from '@material-ui/core/CardHeader';
@@ -8,17 +7,16 @@ import Divider from '@material-ui/core/Divider';
 import axios from 'axios';
 import { withStyles } from '@material-ui/core/styles';
 import Input from '@material-ui/core/Input';
-import InputBase from '@material-ui/core/InputBase';
 import InputLabel from '@material-ui/core/InputLabel';
-import TextField from '@material-ui/core/TextField';
 import FormControl from '@material-ui/core/FormControl';
 import purple from '@material-ui/core/colors/purple';
 import Grid from '@material-ui/core/Grid';
 import IconButton from '@material-ui/core/IconButton';
-import AddBox from '@material-ui/icons/AddBox';
 import Button from '@material-ui/core/Button';
 import DeleteIcon from '@material-ui/icons/DeleteForeverOutlined';
 import CardActions from '@material-ui/core/CardActions';
+import update from 'immutability-helper';
+
 
 const styles = theme => ({
   formControl: {
@@ -65,6 +63,10 @@ const styles = theme => ({
   },
   leftButtons: {
     marginLeft: 'auto'
+  },
+  save: {
+    marginTop: '10px',
+    marginBottom: '10px'
   }
 })
 
@@ -74,18 +76,16 @@ class AddPlans extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: [],
-      groups: ['Starter'],
       planForm: {
         planName: '',
         groups: [
           {
-            title: 'Starter', 
+            title: '', 
             defaultSets: '', 
             exercises: [
-              {name: '', defaultReps: 5}, 
-              {name: '', defaultReps: 5}, 
-              {name: '', defaultReps: 5} 
+              {name: '', defaultReps: ''}, 
+              {name: '', defaultReps: ''}, 
+              {name: '', defaultReps: ''} 
             ]
           }
         ]
@@ -93,122 +93,210 @@ class AddPlans extends React.Component {
     }
   }
 
+  updatePlanName = (e) => {
+    const { planForm } = this.state
+    const copyForm = update(planForm, {planName: {$set: e.target.value}})
+    this.setState({planForm: copyForm})
+  }
+
+  updateGroup = (e, index, prop) => {
+    const { planForm } = this.state
+    const copyForm = update(planForm, {groups:{[index]: {[prop]: {$set: e.target.value}}}})
+    this.setState({planForm: copyForm})
+  }
+
+  updateExercise = (e, groupIndex, exerciseIndex, prop) => {
+    const { planForm } = this.state
+    const copyForm = update(planForm, 
+      {groups:{[groupIndex]: {exercises:{[exerciseIndex]: {[prop]: {$set: e.target.value}}}}}})
+    this.setState({planForm: copyForm})
+  }
+
+  addGroup = () => {
+    const newGroup =   {
+      title: '', 
+      defaultSets: '', 
+      exercises: [
+        {name: '', defaultReps: ''}, 
+        {name: '', defaultReps: ''}, 
+        {name: '', defaultReps: ''} 
+      ]
+    }
+    const { planForm } = this.state
+    const copyForm = update(planForm, {groups: {$push: [newGroup]}})
+    this.setState({planForm: copyForm})
+  }
+
+  removeGroup = (index) => {
+    const { planForm } = this.state
+    const copyForm = update(planForm, {groups: {$splice: [[index, 1]]}})
+    this.setState({planForm: copyForm})
+  }
+
+  addExercise = (groupIndex) => {
+    const newExercise = {name: '', defaultReps: ''}
+    const { planForm } = this.state
+    const copyForm = update(planForm, 
+      {groups: {[groupIndex]: {exercises: {$push: [newExercise]}}}})
+    this.setState({planForm: copyForm})
+  }
+
+  removeExercise = (groupIndex, exerciseIndex) => {
+    const { planForm } = this.state
+    const copyForm = update(planForm, 
+      {groups: {[groupIndex]: {exercises: {$splice : [[exerciseIndex, 1]]}}}})
+      this.setState({planForm: copyForm})
+  }
+
   render() {
     const { classes } = this.props
     return (
       <div>
         <Card>
-          <CardHeader title={ <FormControl>
-            <InputLabel htmlFor="name" classes={{
-                  root: classes.cssLabel,
-                  focused: classes.cssFocused,
-                }}>
-              Plan Name
-            </InputLabel>
-            <Input autoFocus id="name" className={classes.inputView} classes={{
-                    underline: classes.cssUnderline,
-                  }} />
-            </FormControl>
+          <CardHeader 
+            title={ 
+              <FormControl>
+                <InputLabel htmlFor="name" 
+                    classes={{root: classes.cssLabel, focused: classes.cssFocused,}
+                }>
+                  Plan Name
+                </InputLabel>
+                <Input autoFocus id="name" 
+                    className={classes.inputView} 
+                    classes={{underline: classes.cssUnderline}}
+                    onChange={this.updatePlanName} 
+                />
+              </FormControl>
             }
             />
             <Divider variant="middle" />
 
             <CardContent className={classes.cardContent}>
               <Typography align="justify" variant="subtitle1">Groups</Typography>
-              <Divider/>
-              <Button className={classes.buttonAddGroup} variant="outlined" color="primary"  aria-label="AddBox">
+              <Divider />
+              <Button className={classes.buttonAddGroup} 
+                  variant="outlined" 
+                  color="primary" 
+                  aria-label="AddBox"
+                  onClick={this.addGroup}>
                 Add group
               </Button>
-            
-              
+
+
               <div className={classes.root}>
                 <Grid container spacing={24}>
                   {this.state.planForm.groups.map((group, i) => (
-                  <Grid item xl={4} key={group}>
+                  <Grid item xl={4} key={i}>
                     <Card>
-                      <CardHeader className={classes.groupCardHeader} title={ 
-                        <div >
-                          <FormControl className={classes.formControl}>
-                            <InputLabel htmlFor="groupTitle" classes={{
-                                          root: classes.cssLabel,
-                                          focused: classes.cssFocused,
-                                        }}>
-                              Group Name
-                            </InputLabel>
-                            <Input id="groupTitle" className={classes.inputView} classes={{
-                                            underline: classes.cssUnderline,
-                                          }} />
-                          </FormControl>
+                      <CardHeader className={classes.groupCardHeader} 
+                        title={ 
+                          <div>
+                            <FormControl className={classes.formControl}>
+                              <InputLabel htmlFor={`groupTitle${i}`} 
+                                  classes={{root: classes.cssLabel, focused: classes.cssFocused}}
+                              >
+                                Group Name
+                              </InputLabel>
+                              <Input id={`groupTitle${i}`} className={classes.inputView} 
+                                  classes={{underline: classes.cssUnderline}} 
+                                  onChange={(e)=> this.updateGroup(e, i, 'title')}
+                                  value={this.state.planForm.groups[i].title}
+                              />
+                            </FormControl>
 
-                          <FormControl className={classes.formControl}>
-                          <InputLabel htmlFor="setCount" classes={{
-                                        root: classes.cssLabel,
-                                        focused: classes.cssFocused,
-                                      }}>
-                            Sets
-                          </InputLabel>
-                          <Input type ="number" id="setCount" className={classes.defaultSetInputView} classes={{
-                                          underline: classes.cssUnderline,
-                                        }} />
-                          </FormControl>
-                        </div>
+                            <FormControl className={classes.formControl}>
+                              <InputLabel htmlFor="setCount" 
+                                  classes={{root: classes.cssLabel, focused: classes.cssFocused}}
+                              >
+                                Sets
+                              </InputLabel>
+                              <Input type="number" id="setCount" className={classes.defaultSetInputView} 
+                                  classes={{underline: classes.cssUnderline}} 
+                                  onChange={(e)=> this.updateGroup(e, i, 'defaultSets')}
+                                  value={this.state.planForm.groups[i].defaultSets}
+                              />
+                            </FormControl>
+                          </div>
                         }
-                        />
+                      />
 
-                        <Divider variant="middle" />
+                      <Divider variant="middle" />
 
-                        <CardContent >
-                          {this.state.planForm.groups[i].exercises.map(exercise => (
-                            <div>
-                              <IconButton className={classes.delete} aria-label="Delete">
-                                <DeleteIcon />
-                              </IconButton>
-                              <FormControl className={classes.formControl}>
-                                <InputLabel htmlFor="exerciseName" classes={{
-                                              root: classes.cssLabel,
-                                              focused: classes.cssFocused,
-                                            }}>
-                                  Exercise
-                                </InputLabel>
-                                <Input id="exerciseName" className={classes.inputView} classes={{
-                                                underline: classes.cssUnderline,
-                                              }} />
-                              </FormControl>
+                      <CardContent>
+                        {this.state.planForm.groups[i].exercises.map((exercise, idx) => (
+                          <div key={idx}>
+                            <IconButton className={classes.delete} 
+                                aria-label="Delete" 
+                                onClick={()=> {this.removeExercise(i, idx)}}
+                            >
+                              <DeleteIcon />
+                            </IconButton>
+                            <FormControl className={classes.formControl}>
+                              <InputLabel htmlFor="exerciseName" 
+                                  classes={{root: classes.cssLabel, focused: classes.cssFocused}}
+                              >
+                                Exercise
+                              </InputLabel>
+                              <Input id="exerciseName" className={classes.inputView} 
+                                  classes={{underline: classes.cssUnderline}} 
+                                  onChange={(e)=> this.updateExercise(e, i, idx, 'name')}
+                                  value={this.state.planForm.groups[i].exercises[idx].name}
+                              />
+                            </FormControl>
 
-                              <FormControl className={classes.formControl}>
-                                <InputLabel htmlFor="defaultReps" classes={{
-                                              root: classes.cssLabel,
-                                              focused: classes.cssFocused,
-                                            }}>
-                                  Reps
-                                </InputLabel>
-                                <Input type ="number" id="defaultReps" className={classes.defaultSetInputView} classes={{
-                                                underline: classes.cssUnderline,
-                                              }} />
-                          
-                              </FormControl>
-                            </div>
-                          ))}
+                            <FormControl className={classes.formControl}>
+                              <InputLabel htmlFor="defaultReps" 
+                                  classes={{root: classes.cssLabel, focused: classes.cssFocused}}
+                              >
+                                Reps
+                              </InputLabel>
+                              <Input type="number" id="defaultReps" 
+                                  className={classes.defaultSetInputView} 
+                                  onChange={(e) => this.updateExercise(e, i, idx, 'defaultReps')}
+                                  value={this.state.planForm.groups[i].exercises[idx].defaultReps}
+                                  classes={{underline: classes.cssUnderline,}}
+                              />
+
+                            </FormControl>
+                          </div>
+                        ))}
                         <CardActions disableActionSpacing>
-                          <Button className={classes.leftButtons} variant="outlined" color="primary"  aria-label="AddBox" size="small">
+                          <Button className={classes.leftButtons} 
+                              variant="outlined" 
+                              color="primary" 
+                              aria-label="AddBox"
+                              size="small" 
+                              onClick={()=> this.addExercise(i)}
+                            >
                             Add Exercise
                           </Button>
                         </CardActions>
                       </CardContent>
-                      <Divider/>
-                      
+                    
+                      <Divider />
+
                       <CardActions>
-                        <Button size="small" color="primary" className={classes.leftButtons}>
+                        <Button className={classes.leftButtons} 
+                            size="small" 
+                            color="default" 
+                            onClick={()=> this.removeGroup(i)}
+                        >
                           Remove Group
                         </Button>
                       </CardActions>
                     </Card>
-                  </Grid>
+                    </Grid>
                   ))}
                 </Grid>
               </div>
-
             </CardContent>
+            <Divider />
+            <CardActions>
+              <Button className={classes.save} 
+                  color="primary" 
+                  variant="contained" 
+                  size="large">submit plan</Button>
+            </CardActions>
         </Card>
       </div>
     )
